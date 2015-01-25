9b1c056857d28e0accccf7d6596f2c1c42f6cada
Jakub Kalina <jakub.kalina@allegro.pl>
2015-01-25 15:44:34 +0100

    Polish translation of gallery plugin

---
+++ core/components/gallery/lexicon/pl/tv.inc.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Gallery
+ *
+ * Copyright 2010-2012 by Shaun McCormick <shaun@modx.com>
+ *
+ * Gallery is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Gallery is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Gallery; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package gallery
+ */
+/**
+ * Custom TV Polish Lexicon Entries for Gallery
+ * Translated by Jakub Kalina (modx@jakubkalina.pl)
+ *
+ * @package gallery
+ * @subpackage lexicon
+ */
+$_lang['gallery.active'] = 'Aktywny';
+$_lang['gallery.alt_text'] = 'Tekst alternatywny (alt)';
+$_lang['gallery.browser'] = 'Przeglądarka galerii';
+$_lang['gallery.choose_item'] = 'Wybierz obraz';
+$_lang['gallery.class'] = 'Klasy CSS';
+$_lang['gallery.clear_image'] = 'Wyczyść obraz';
+$_lang['gallery.crop_bottom'] = 'Przytnij z dołu';
+$_lang['gallery.crop_enable'] = 'Aktywuj przycinanie';
+$_lang['gallery.crop_left'] = 'Przytnij z lewej';
+$_lang['gallery.crop_right'] = 'Przytnij z prawej';
+$_lang['gallery.crop_top'] = 'Przytnij z góry';
+$_lang['gallery.file_name'] = 'Nazwa pliku';
+$_lang['gallery.file_size'] = 'Wielkość pliku';
+$_lang['gallery.height'] = 'Wysokość';
+$_lang['gallery.inactive'] = 'Nieaktywny';
+$_lang['gallery.other_opt'] = 'Inne opcje phpThumb';
+$_lang['gallery.other_opt_desc'] = 'Inne parametry konfiguracyjne, które chcesz przekazać do phpThumb. Zapisz je w formie URL.';
+$_lang['gallery.pos_b'] = 'Dolny';
+$_lang['gallery.pos_bl'] = 'Lewy dolny';
+$_lang['gallery.pos_br'] = 'Prawy dolny';
+$_lang['gallery.pos_c'] = 'Środek';
+$_lang['gallery.pos_l'] = 'Lewo';
+$_lang['gallery.pos_r'] = 'Prawo';
+$_lang['gallery.pos_t'] = 'Góra';
+$_lang['gallery.pos_tr'] = 'Prawy górny';
+$_lang['gallery.pos_tl'] = 'Lewy górny';
+$_lang['gallery.resize'] = 'Zmień rozmiar';
+$_lang['gallery.rotate'] = 'Obróć';
+$_lang['gallery.tags'] = 'Znaczniki';
+$_lang['gallery.title'] = 'Tytuł';
+$_lang['gallery.watermark_options'] = 'Opcje znaku wodnego';
+$_lang['gallery.watermark_options_desc'] = 'Uwaga: Te opcje nie są widoczne w trybie podglądu.';
+$_lang['gallery.watermark_text'] = 'Tekstowy znak wodny';
+$_lang['gallery.watermark_text_desc'] = 'Tekst, który ma się pojawić na obrazie jako znak wodny.';
+$_lang['gallery.watermark_text_position'] = 'Pozycja znaku wodnego';
+$_lang['gallery.watermark_text_position_desc'] = 'Pozycja tekstu znaku wodnego na obrazie.';
+$_lang['gallery.width'] = 'Szerokość';
---
+++ core/components/gallery/lexicon/pl/tvprops.inc.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Gallery
+ *
+ * Copyright 2010-2012 by Shaun McCormick <shaun@modx.com>
+ *
+ * Gallery is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Gallery is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Gallery; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package gallery
+ */
+/**
+ * Custom TV Polish Lexicon Entries for Gallery TVs
+ * Translated by Jakub Kalina (modx@jakubkalina.pl)
+ *
+ * @package gallery
+ * @subpackage lexicon
+ */
+$_lang['galtv.ascending'] = 'Rosnąco';
+$_lang['galtv.descending'] = 'Malejąco';
+$_lang['galtv.limit'] = 'Limit';
+$_lang['galtv.limit_desc'] = 'Limit wyświetlanych albumów. Ustaw 0 aby wyświetlić wszystkie.';
+$_lang['galtv.listwidth'] = 'Szerokość listy albumów na liście rozwijanej';
+$_lang['galtv.listwidth_desc'] = 'Szerokość listy albumów na liście rozwijanej wyrażona w pikselach, domyślnie jest to szerokość listy.';
+$_lang['galtv.maxheight'] = 'Maksymalna wysokość listy rozwijanej';
+$_lang['galtv.maxheight_desc'] = 'Maksymalna wysokość listy rozwijanej wyrażona w pikselach.';
+$_lang['galtv.name'] = 'Nazwa';
+$_lang['galtv.parent'] = 'Album nadrzędny';
+$_lang['galtv.parent_desc'] = 'Jeśli ustawione, użyte zostaną tylko albumy bezpośrednio potomne do wskazanego albumu. Ustaw
+pustą wartość aby pokazać wszystkie lub 0, aby ograniczyć do albumów najwyższego poziomu.';
+$_lang['galtv.rank'] = 'Ranga';
+$_lang['galtv.showcover'] = 'Pokaż okładkę';
+$_lang['galtv.showcover_desc'] = 'Pokaż miniaturę okładki przy albumie.';
+$_lang['galtv.shownone'] = 'Pokaż opcję "brak"';
+$_lang['galtv.shownone_desc'] = 'Pokaż na liścię opcję brak odpowiadającą pustej wartości.';
+$_lang['galtv.sort'] = 'Sortuj według';
+$_lang['galtv.sort_desc'] = 'Polem według którego sortować albumy.';
+$_lang['galtv.sortdir'] = 'Kierunek sortowania';
+$_lang['galtv.sortdir_desc'] = 'Określa kierunek posortowania albumów.';
+$_lang['galtv.start'] = 'Indeks początkowy';
+$_lang['galtv.start_desc'] = 'Jeśli określony jest limit, wyświetli począwszy od wskazanego indeksu.';
+$_lang['galtv.width'] = 'Szerokość listy rozwijanej';
+$_lang['galtv.width_desc'] = 'Szerokość listy rozwijanej wyrażona w pikselach.';
+$_lang['galtv.subchilds'] = 'Pokaż wszystkie albumy potomne';
+$_lang['galtv.subchilds_desc'] = 'Pokaż wszystkie galerie potomne, zarówno bezpośrednio jak i pośrednio';
