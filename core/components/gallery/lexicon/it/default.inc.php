2b7b8715ffa333381673e37567acadf76bfb952c
FerX <fernando.figaroli@gmail.com>
2016-08-14 19:54:49 +0200

    Italian translation

---
+++ core/components/gallery/lexicon/it/default.inc.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Gallery
+ *
+ * Copyright 2010-2012 by Shaun McCormick <shaun@modx.com>
+ * Transate by FerX https://github.com/FerX
+ *
+ * Gallery is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Gallery is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Gallery; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package gallery
+ */
+/**
+ * Default Italian Lexicon Entries for Gallery
+ *
+ * @package gallery
+ * @subpackage lexicon
+ */
+$_lang['gallery'] = 'Galleria';
+$_lang['gallery.year'] = 'Anno';
+$_lang['gallery.active'] = 'Attiva';
+$_lang['gallery.active_desc'] = 'Se non spuntato, questo album non &egrave; visibile.';
+$_lang['gallery.album'] = 'Album';
+$_lang['gallery.album_create'] = 'Crea un Album';
+$_lang['gallery.album_err_nf'] = 'Album non trovato.';
+$_lang['gallery.album_err_ns'] = 'Album non specificato.';
+$_lang['gallery.album_err_ns_name'] = 'Si prega di inserire un nome valido per l\'album.';
+$_lang['gallery.album_err_remove'] = 'Si &egrave; verificato un errore mentre provavo ad eliminare l\'album.';
+$_lang['gallery.album_err_save'] = 'Si &egrave; verificato un errore mentre provavo a salvare l\'album..';
+$_lang['gallery.album_remove'] = 'Elimina l\'Album';
+$_lang['gallery.album_remove_confirm'] = 'Sei sicuro di voler eliminare questo album? Tutti gli elementi che non sono in nessun altro album saranno rimossi.';
+$_lang['gallery.album_update'] = 'Aggiorna l\'Album';
+$_lang['gallery.albums'] = 'Album';
+$_lang['gallery.back'] = 'Indietro';
+$_lang['gallery.batch_upload'] = 'Batch Upload';
+$_lang['gallery.batch_upload_intro'] = 'Specificare una directory sul file system per la ricerca delle immagini. &Egrave; possibile utilizzare {base_path}, {core_path}, o {assets_path} come placeholders.';
+$_lang['gallery.batch_upload_tags'] = 'Etichette da assegnare ad ogni oggetto caricato in batch, in elenco separato da virgole.';
+$_lang['gallery.bytes'] = 'bytes';
+$_lang['gallery.comma_separated_list'] = 'Lista separata da virgola';
+$_lang['gallery.clearsuccessful'] = 'Cancella gli Upload terminati con successo';
+$_lang['gallery.clearfailure'] = 'Cancella gli upload falliti';
+$_lang['gallery.cover_filename'] = 'Copertina dell\'Album';
+$_lang['gallery.delete_cover'] = 'Cancella la copertina';
+$_lang['gallery.directory'] = 'Directory';
+$_lang['gallery.directory_desc'] = 'la directory per la ricerca delle immagini.';
+$_lang['gallery.directory_err_create'] = 'Impossibile creare la directory: [[+directory]]';
+$_lang['gallery.directory_err_nf'] = 'Directory non trovata.';
+$_lang['gallery.directory_err_ns'] = 'Directory not specificata.';
+$_lang['gallery.directory_err_write'] = 'Impossibile scrivere nella directory: [[+directory]]';
+$_lang['gallery.file'] = 'File';
+$_lang['gallery.file_err_move'] = 'Si &egrave; verificato un errore durante il tentativo di spostare il file: [[+file]] in [[+target]]';
+$_lang['gallery.file_name'] = 'Nome File';
+$_lang['gallery.file_size'] = 'Dimensione File';
+$_lang['gallery.height'] = 'Altezza';
+$_lang['gallery.images_selected'] = '[[+count]] immagini selezionate.';
+$_lang['gallery.inactive'] = 'Inattivo';
+$_lang['gallery.intro_msg'] = 'Qui &egrave; possibile gestire i tuoi album. Fai clic destro su un album per visualizzare pi&ugrave; opzioni.';
+$_lang['gallery.item_active_desc'] = 'Gli elementi inattivi non verranno mostrati pubblicamente negli album.';
+$_lang['gallery.item_delete'] = 'Cancella l\'elemento';
+$_lang['gallery.item_delete_confirm'] = 'Sei sicuro di voler eliminare tutto l\'elemento? Operazione irreversibile.';
+$_lang['gallery.item_delete_multiple'] = 'Cancella gli Elementi Selezionati';
+$_lang['gallery.item_delete_multiple_confirm'] = 'Sei sicuro di voler eliminare questi elementi? Operazione irreversible.';
+$_lang['gallery.item_err_nf'] = 'Elemento non trovato.';
+$_lang['gallery.item_err_ns'] = 'Elemento non specificato.';
+$_lang['gallery.item_err_ns_file'] = 'Si prega di specificare un file da caricare.';
+$_lang['gallery.item_err_remove'] = 'Si &egrave; verificato un errore durante il tentativo di rimuovere l\'elemento.';
+$_lang['gallery.item_err_save'] = 'Si &egrave; verificato un errore durante il tentativo di salvare l\'elemento.';
+$_lang['gallery.item_err_upload'] = 'Si &egrave; verificato un errore durante il tentativo di caricare l\'elemento.';
+$_lang['gallery.item_remove'] = 'Rimuovi l\'Elemento';
+$_lang['gallery.item_remove_album'] = 'Rimuovi l\'Elemento dall\'Album';
+$_lang['gallery.item_update'] = 'Aggiorna l\'Elemento';
+$_lang['gallery.item_upload'] = 'Carica l\'Elemento';
+$_lang['gallery.item_upload_file_desc'] = 'Il file dell\'immagine che si desidera caricare.';
+$_lang['gallery.item_url'] = 'URL';
+$_lang['gallery.item_url_desc'] = 'URL di destinazione quando si far&agrave; click su questo elemento dell\'Album.';
+$_lang['gallery.items'] = 'Elementi';
+$_lang['gallery.loading_ellipsis'] = 'Caricando...';
+$_lang['gallery.menu_desc'] = 'Un sistema di galleria dinamica.';
+$_lang['gallery.multi_item_upload'] = 'Multi-Caricamento';
+$_lang['gallery.parent'] = 'Genitore';
+$_lang['gallery.prominent'] = 'Evidente';
+$_lang['gallery.prominent_desc'] = 'Fare un album non evidente pu&ograve; essere utilizzato per nascondere gli album dal vostro elenco Album, se si vuole un album privato, o per creare album non visibile.';
+$_lang['gallery.refresh'] = 'Aggiorna';
+$_lang['gallery.set_as_cover'] = 'Imposta come copertina dell\'Album';
+$_lang['gallery.tags'] = 'Etichette';
+$_lang['gallery.title'] = 'Titolo';
+$_lang['gallery.upload'] = 'Carica';
+$_lang['gallery.upload_cover'] = 'Carica copertina';
+$_lang['gallery.view_cover'] = 'Mostra copertina';
+$_lang['gallery.width'] = 'Larghezza';
+$_lang['gallery.xpdozip_err_nf'] = 'Impossibile caricare la classe xPDOZip.';
+$_lang['gallery.zip_err_ns'] = 'Si prega di specificare un file zip.';
+$_lang['gallery.zip_err_unpack'] = 'Impossibile decomprimere il file zip. Si prega di verificare e assicurarsi che il file zip non sia danneggiato e che il percorso file &egrave; corretto.';
+$_lang['gallery.zip_file'] = 'Zip File';
+$_lang['gallery.zip_upload'] = 'Carica Zip';
+$_lang['gallery.zip_upload_intro'] = 'Specificare un file zip da caricare. Galleria decomprimer&agrave; il file e inserir&agrave; le immagini in questo album.';
+
+
+$_lang['area_backend'] = 'Backend';
+
+$_lang['setting_gallery.backend_thumb_far'] = 'Backend Thumbnail Aspect Ratio';
+$_lang['setting_gallery.backend_thumb_far_desc'] = 'The phpThumb FAR (aspect ratio) setting for thumbnails when managing them in the backend.';
+
+$_lang['setting_gallery.backend_thumb_height'] = 'Backend Thumbnail Height';
+$_lang['setting_gallery.backend_thumb_height_desc'] = 'The height in pixels for thumbnails when managing them in the backend.';
+
+$_lang['setting_gallery.backend_thumb_width'] = 'Backend Thumbnail Width';
+$_lang['setting_gallery.backend_thumb_width_desc'] = 'The width in pixels for thumbnails when managing them in the backend.';
+
+$_lang['setting_gallery.backend_thumb_zoomcrop'] = 'Backend Thumbnail Zoomcrop';
+$_lang['setting_gallery.backend_thumb_zoomcrop_desc'] = 'Whether or not to use zoomcrop for thumbnails when managing them in the backend.';
+
+$_lang['setting_gallery.default_batch_upload_path'] = 'Default Batch Upload Path';
+$_lang['setting_gallery.default_batch_upload_path_desc'] = 'The default value to use for the path when using batch upload.';
+
+$_lang['setting_gallery.thumbs_prepend_site_url'] = 'Prepend Site URL to Thumbs';
+$_lang['setting_gallery.thumbs_prepend_site_url_desc'] = 'If true, will prepend the site URL to all thumbnails being sent to phpThumb.';
---
+++ core/components/gallery/lexicon/it/tv.inc.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Gallery
+ *
+ * Copyright 2010-2012 by Shaun McCormick <shaun@modx.com>
+ * Transate by FerX https://github.com/FerX
+ *
+ * Gallery is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Gallery is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Gallery; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package gallery
+ */
+/**
+ * Custom TV Italian Lexicon Entries for Gallery
+ *
+ * @package gallery
+ * @subpackage lexicon
+ */
+$_lang['gallery.active'] = 'Attiva';
+$_lang['gallery.alt_text'] = 'Testo Alternativo';
+$_lang['gallery.browser'] = 'Visualizzatore della Galleria';
+$_lang['gallery.choose_item'] = 'Scegli un immagine';
+$_lang['gallery.class'] = 'Classi CSS';
+$_lang['gallery.clear_image'] = 'Pulisci l\'immagine';
+$_lang['gallery.crop_bottom'] = 'Ritaglia in Basso';
+$_lang['gallery.crop_enable'] = 'Attiva il ritaglio';
+$_lang['gallery.crop_left'] = 'Ritaglia a Sinistra';
+$_lang['gallery.crop_right'] = 'Ritaglia a Destra';
+$_lang['gallery.crop_top'] = 'Ritaglia in Alto';
+$_lang['gallery.file_name'] = 'Nome del File';
+$_lang['gallery.file_size'] = 'Dimensione del File';
+$_lang['gallery.height'] = 'Altezza';
+$_lang['gallery.inactive'] = 'Itattiva';
+$_lang['gallery.other_opt'] = 'Ulteriori phpThumb Opzioni';
+$_lang['gallery.other_opt_desc'] = 'Per passare ulteriori parametri di configurazione a phpThumb si prega di utilizzare il formato URL.';
+$_lang['gallery.pos_b'] = 'Basso';
+$_lang['gallery.pos_bl'] = 'Basso a Sinistra';
+$_lang['gallery.pos_br'] = 'Basso a Destra';
+$_lang['gallery.pos_c'] = 'Centro';
+$_lang['gallery.pos_l'] = 'Sinistra';
+$_lang['gallery.pos_r'] = 'Destra';
+$_lang['gallery.pos_t'] = 'Alto';
+$_lang['gallery.pos_tr'] = 'Alto a Destra';
+$_lang['gallery.pos_tl'] = 'Alto a Sinistra';
+$_lang['gallery.resize'] = 'Ridimensiona';
+$_lang['gallery.rotate'] = 'Ruota';
+$_lang['gallery.tags'] = 'Etichette';
+$_lang['gallery.title'] = 'Titolo';
+$_lang['gallery.watermark_options'] = 'Opzioni Watermark';
+$_lang['gallery.watermark_options_desc'] = 'Nota: queste opzioni non possono essere visualizzate in modalità anteprima';
+$_lang['gallery.watermark_text'] = 'Testo Watermark';
+$_lang['gallery.watermark_text_desc'] = 'Testo da inserire sull\'immagine come watermark.';
+$_lang['gallery.watermark_text_position'] = 'Posizione del testo Watermark';
+$_lang['gallery.watermark_text_position_desc'] = 'La posizione del testo watermark.';
+$_lang['gallery.width'] = 'Larghezzza';
