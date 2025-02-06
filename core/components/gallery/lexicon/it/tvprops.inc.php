fd4310cbc3423c601f0565f8d5563cfa687d4ed6
SilverSocmel <silver.socmel@miapostaweb.it>
2025-02-06 10:52:05 +0100

    Create tvprops.inc.php

---
+++ core/components/gallery/lexicon/it/tvprops.inc.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Gallery
+ *
+ * Copyright 2010-2012 by Shaun McCormick <shaun@modx.com>
+ *
+ * Gallery is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Gallery is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Gallery; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package gallery
+ */
+/**
+ * Custom TV Italian Lexicon Entries for Gallery TVs  (by SilverMabol)
+ *
+ * @package gallery
+ * @subpackage lexicon
+ */
+$_lang['galtv.ascending'] = 'ASC Ascendente';
+$_lang['galtv.descending'] = 'DESC Discendente';
+$_lang['galtv.limit'] = 'Limita';
+$_lang['galtv.limit_desc'] = 'Il numero massimo degli Albums di Gallery da visualizzare. Imposta <b>0</b> per nessun limte (TUTTI).';
+$_lang['galtv.listwidth'] = 'Larghezza Lista';
+$_lang['galtv.listwidth_desc'] = 'La larghezza, in pixel, del menu a tendina, lista.';
+$_lang['galtv.maxheight'] = 'Massima Altezza Lista';
+$_lang['galtv.maxheight_desc'] = 'Massima Altezza, in pixel, del menu a tendina, lista.';
+$_lang['galtv.name'] = 'Nome';
+$_lang['galtv.parent'] = 'Album Genitore';
+$_lang['galtv.parent_desc'] = 'ID Album Genitore. Se impostato, utilizzerà solo gli album che sono figli diretti album con ID specificato. Imposta su <b>(vuoto)</b> per mostrare tutto, <b>0</b> per mostrare solo albums di livello massimo (principali).';
+$_lang['galtv.rank'] = 'Rango';
+$_lang['galtv.showcover'] = 'Mostra Cover';
+$_lang['galtv.showcover_desc'] = 'Mostra miniatura degli albuno fi fianco al nome nel menu a tendina.';
+$_lang['galtv.shownone'] = 'Mostra Opzione "Nessuno"';
+$_lang['galtv.shownone_desc'] = 'Mostra opzione (nessuno) nella lista che può essere scelta per non selezionare nessun album.';
+$_lang['galtv.sort'] = 'Ordina Per';
+$_lang['galtv.sort_desc'] = 'Il Campo da utilizzare per ordinare gli elementi della lista.';
+$_lang['galtv.sortdir'] = 'Direzione Ordine';
+$_lang['galtv.sortdir_desc'] = 'La direzione di ordinamento da utilizzare col campo per ordinare lista Albums.';
+$_lang['galtv.start'] = 'Index Iniziale';
+$_lang['galtv.start_desc'] = 'Se "Limita" è mggiore di 0, si partirà da questo index.';
+$_lang['galtv.width'] = 'Larghezza';
+$_lang['galtv.width_desc'] = 'La larghessa della combobox, in pixels.';
+$_lang['galtv.subchilds'] = 'Mostra tutti i figli del genitore';
+$_lang['galtv.subchilds_desc'] = 'Mostra tutti i figli e i sottofigli del genitore.';
